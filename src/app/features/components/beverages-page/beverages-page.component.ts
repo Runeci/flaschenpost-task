1e1aa5e12b58da1c905e805857f198dd5f506af0
Runeci <kostenokd@mail.ru>
2022-07-27 12:16:56 +0300

    feat: set up basic project structure

---
+++ src/app/features/components/beverages-page/beverages-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-features',
+  templateUrl: './beverages-page.component.html',
+  styleUrls: ['./beverages-page.component.scss']
+})
+export class BeveragesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/models/beverages.interfaces.ts
@@ -0,0 +1,16 @@
+export interface Beverage {
+  readonly id: number,
+  readonly brandName: string,
+  readonly name: string,
+  readonly descriptionText?: string,
+  readonly articles: Articles[],
+}
+
+interface Articles {
+  readonly id: number,
+  readonly shortDescription: string,
+  readonly price: number,
+  readonly unit: string,
+  readonly pricePerUnitText: string,
+  readonly image: string,
+}
